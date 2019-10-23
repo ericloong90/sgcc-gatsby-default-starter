420f8f36d1dc7104f6756d786a3a1235cb2ee54d
Eric Tai <eric.loong@sgcodecampus.com>
2019-10-23 17:58:11 +0800

    Updated useOutsideClick hook to now take in an array of refs to avoid instead of just one ref

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -44,12 +44,40 @@ const isDesktop = () => window.matchMedia($.device.desktop).matches;
 
 /**
  * Informs if a click outside the given ref is done.
- * @param {React.reference} ref - Reference to the component
- * @param {func} callback - A callback function to run if user clicked outside the component
+ * @param {array} refArray - Array of component refs to avoid
+ * @param {func} callback - A callback function to run if user clicked outside the array of
+ *  components
  */
-const useOutsideClick = (ref, callback) => {
+const useOutsideClick = (refArray, callback) => {
   const handleOutsideClick = (event) => {
-    if (ref.current && !ref.current.contains(event.target)) {
+    let outsideClick = false;
+
+    if (!(Array.isArray(refArray) && refArray.length > 0)) {
+      console.log('Please pass in an array of refs.');
+    }
+
+    refArray.forEach((ref) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        if (refArray.length > 1) {
+          refArray
+            .filter((refPrime) => {
+              if (ref === refPrime) {
+                return false;
+              }
+              return true;
+            })
+            .forEach((refPrime) => {
+              if (!refPrime.current.contains(event.target)) {
+                outsideClick = true;
+              }
+            });
+        } else {
+          outsideClick = true;
+        }
+      }
+    });
+
+    if (outsideClick) {
       callback(event);
     }
   };
